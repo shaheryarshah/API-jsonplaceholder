ccaca817d52b8c5c79aad86b9cf27b992dca2126
shaheryarshah <shaheryarshah2022@gmail.com>
2024-12-22 03:10:49 -0800

    first commit

---
+++ src/app/[dr]/page.tsx
@@ -0,0 +1,11 @@
+import exp from "constants"
+
+const DynamicRout = (props:any) =>{
+console.log(props)
+    return(
+        <h1>{`${props.params.dr}`}</h1>
+
+        
+    )
+}
+export default DynamicRout
